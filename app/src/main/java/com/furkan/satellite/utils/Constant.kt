0f0700aa9bb3367ef47c50e326c3c7d7821889fd
df.furkan.aksu <112544063+aksufurkann@users.noreply.github.com>
2023-10-17 01:32:45 +0300

    Create Constant.kt

---
+++ app/src/main/java/com/furkan/satellite/utils/Constant.kt
@@ -0,0 +1,11 @@
+package com.furkan.satellite.utils
+
+object Constant {
+    const val IO_ERROR = "IO_ERROR"
+    const val LIST_EMPTY_ERROR = "LIST_EMPTY_ERROR"
+    const val SATELLITE_FILE = "satellite-list.json"
+    const val SATELLITE_DETAIL_FILE = "satellite-detail.json"
+    const val SATELLITE_POSITION_FILE = "positions.json"
+    const val POSITION_CHANGE_DELAY = 3000L
+    const val SEARCH_DELAY = 500L
+}
