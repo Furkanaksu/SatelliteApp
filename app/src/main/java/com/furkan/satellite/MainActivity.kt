ff99a3759083650131a5b3429bc0b4c8301ff802
df.furkan.aksu <112544063+aksufurkann@users.noreply.github.com>
2023-10-18 05:09:10 +0300

    themes updated

--- app/src/main/java/com/furkan/satellite/MainActivity.kt
+++ app/src/main/java/com/furkan/satellite/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.furkan.satellite.features.screens.navigation.NavGraph
+import com.furkan.satellite.features.navigation.NavGraph
+import com.furkan.satellite.ui.theme.Colors
 import com.furkan.satellite.ui.theme.SatelliteAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,7 +21,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier
                         .fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Colors.White
                 ) {
                     NavGraph()
                 }
